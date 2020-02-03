9131b1099d2799fcbff14bdf7ebbcd1f5ede6a3c
Kevin <Atnnetworkk@gmail.com>
2020-02-03 01:47:15 +0100

    Call a prop result

--- src/components/result/index.jsx
+++ src/components/result/index.jsx
@@ -3,15 +3,12 @@ import React from 'react';
 class Result extends React.Component{
     constructor(props){
         super(props)
-        this.state = {
-            result: 0,
-        }
     }
     render(){
         return(
             <div className="result">
                 <p className="result__text">The price is</p>
-                <p className="result__number">{this.state.result}</p>
+                <p className="result__number">{this.props.result}</p>
             </div>
         )
     }
