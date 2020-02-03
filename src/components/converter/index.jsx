a860176a154b67df65ae35aae22ec73621fe582d
Kevin <Atnnetworkk@gmail.com>
2020-02-03 01:47:48 +0100

    Add state and inputhandler

--- src/components/converter/index.jsx
+++ src/components/converter/index.jsx
@@ -1,19 +1,30 @@
 // Components/Converter
 import React from "react";
+import Result from "../result";
 
 class Converter extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            percentage: this.props.percentage,
+            price: this.props.price,
+        }
     }
-
+    inputHandler(key,value){
+        this.setState({
+            [key]: value
+        });
+    }
     render(){
+        console.log(this.state.percentage);
+        console.log(this.state.price);
         return(
             <form className="converter">
                 <label className="converter__label" htmlFor="percentage">What does</label>
-                <input className="converter__inputs" id="percentage" type="number" placeholder="Enter percentage"/>
+                <input value={this.state.percentage} className="converter__inputs" id="percentage" type="number" placeholder="Enter percentage..." onChange={e => this.inputHandler("percentage",e.target.value)}/>
                 <label className="converter__label" htmlFor="price">of</label>
-                <input className="converter__inputs" id="price" type="number" placeholder="Enter the price"/>
-                <button className="converter__btn">Calculate</button>
+                <input value={this.state.price} className="converter__inputs" id="price" type="number" placeholder="Enter the price... " onChange={e => this.inputHandler("price",e.target.value)}/>
+                <button className="converter__btn" onClick={e => this.props.calculate(this.state.percentage, this.state.price, e)}>Calculate</button>
             </form>
         )
     }
