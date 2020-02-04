b5dbcb962af0e43672334fbd3e7fd1377bf9f413
Kevin <Atnnetworkk@gmail.com>
2020-02-04 18:32:26 +0100

    Updates

---
+++ precache-manifest.172fb9c7683c72f4652250a18303cb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3bcb32741fdbcbd08487a2b27cfe3f",
+    "url": "/Sales-Go/index.html"
+  },
+  {
+    "revision": "d5394911601d313e3ea0",
+    "url": "/Sales-Go/static/css/main.b968508f.chunk.css"
+  },
+  {
+    "revision": "3d81557a59e920be3815",
+    "url": "/Sales-Go/static/js/2.9586b86d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Sales-Go/static/js/2.9586b86d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5394911601d313e3ea0",
+    "url": "/Sales-Go/static/js/main.b349b499.chunk.js"
+  },
+  {
+    "revision": "cbd3a6ec9e0e1fc89e10",
+    "url": "/Sales-Go/static/js/runtime-main.1a6fe0c9.js"
+  }
+]);
